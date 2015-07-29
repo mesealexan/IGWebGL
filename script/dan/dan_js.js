5e5ee42fcb9b4516dff63b2f37cf8db01bd1dc4e
Dan Gepner <dGepner@Reactron.com>
2015-07-29 11:56:12 -0500

    snapSVG test with basic load and HUD navigation pads. Folders cleaned up

--- script/dan/dan_js.js
+++ script/dan/dan_js.js
@@ -1,99 +1,153 @@
-var navOpen = 0;
-
-//HIDE TEXT ON START
-// SEQUENCE OPEN
-//BLINK  SELECTED AND CLOSE
-// FLASH OR shine on Burger hover
-// MOve HUD down by % height screen
-
-//Make % of screen width - with minimum size
-
-//Cool Minority report grid in back (maybe in centered container without hide
 
 
-function hasClass(element, cls) {
-    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
-}
-
-
-$('.burger').show();
-
-function openNav() {  
-         navOpen = 1;
-$('.burger').hide();
-   $('.close-icon').show();
-//$('.burger').animate({  top:'-40px'}, 2000, "easeOutBack");
+function startSVG() {
+  var g;
+    var burger;
+    var hit_btn;
+    var closeIcon;
+    var all_nav_pads;
+    var padHolder;
+    var padMask;
     
-    $("#hud-copy").delay(700).fadeIn(300)
-    $(".nav_x002D_pad_selected").attr("class", "nav_x002D_pad_selected on");
-    $(".nav_x002D_pad").each(function(index,item){
+    var padFill = "#444444";
+    var padHover = "#d37032";
+    var padSelected = "#aed332";
+    
+    
+//OPENCLOSE
+    
+       //add hoverts to PADS
+       // var hoverover = function() { g.animate({ transform: 'S0.7' }, 1000, mina.bounce ) };
+        //var hoverout = function() { g.animate({ transform: 'S1' }, 1000, mina.bounce ) };
         
+       //  g.hover( hoverover, hoverout );
+    
+    
+function openNav() {
+   
+     $("#svg-top").removeClass("closed");
+     burger.attr({visibility: "hidden"});
+    
+    //$("#hud-copy").fadeIn(2000);
+    padMask.transform("S1,1");
+    
+    all_nav_pads.forEach( function(elem,i) {
+        
+        elem.stop().attr({fill:"#ffffff", "fill-opacity": 0 })
+         .animate({ fill: padFill, "fill-opacity": 1 }, ( 200 * i ), mina.bounce )
+    
+     });
+    
+    
+}
+    
+    function closeNav() {
+        
+        function closePart2 () {
+        $("#svg-top").addClass("closed");
+        burger.attr({visibility: "visible"});
+        }
+     
+        
+      padMask.stop().animate({ transform: "S0,1,1200,400" }, 600, mina.easeinout, closePart2 )
+    }
+    
+function toggleNav() {
+       
+    if ( $("#svg-top").hasClass("closed") ) { openNav() } else { closeNav() };
+   
+    }
+    function delayClose() {
+        setTimeout(function () { closeNav(); }, 200);
+    }
+    
+    
+//LOAD TOP SVG
+    
+    var topHUD = Snap('#svg-top-svg');
+    
+    Snap.load("rawSVG/hud_top_pads.svg", function (f) {
+    
+       
+    //GRAB MASTER G
+    g = f.select("#STATIC");
+        
+    //ASSIGN VARS
+    burger = g.select("#burger"); 
+    hit_btn = g.select("#burger-hit"); 
+    close_btn = g.select("#close-icon"); 
+    all_nav_pads = g.selectAll('.nav_pad');
+    padHolder = g.select("#navPads");
+    
+    //HIT TOGGLE BTN
+    hit_btn.click( toggleNav );
+        
+    //add to scene
+    topHUD.append(g);
+        
+    //
+        svgBlur = g.filter(Snap.filter.blur(3, 3));
+        svgShadow = g.filter(Snap.filter.shadow(0, 2, 3));
+        svgNone = g.filter(Snap.filter.shadow(0,0,0));
+        filterAnimRef = svgBlur.node.firstChild;
       
-        
-        $(this).attr("class", "nav_x002D_pad on");
+   //mask rect
+   padMask = g.rect(0, 0, 1260, 708).attr({fill: "#FFFFFF"});
+padHolder.attr({ mask: padMask});
    
         
         
-     
-
-    }) ;
+    // and for each one we add the desired interactivity
+     all_nav_pads.forEach( function(elem,i) {
+         
+    // change the color on mouse over
+    elem.mouseover(function(){
+       this.stop().animate({ fill: padHover, transform: 'S.95, .95' }, 300, mina.elastic );
         
+       
+    });
+    // restore the color on mouse out
+    elem.mouseout(function(){
+       this.stop().animate({fill: padFill }, 600, mina.easein).animate({  transform: 'S1, 1' }, 500, mina.bounce );
+       
+    });
+    // open the popup on click or whatever
+    elem.click(function(){
+       
+        all_nav_pads.forEach( function(elem,i) { 
+            
+            elem.removeClass("selected").attr("filter", svgNone); 
+            elem.stop().animate({ fill: padFill }, 500, mina.easeOut );
+                                               
+                                               });
+        elem.addClass("selected").animate({ fill: padSelected  }, 500, mina.easein, delayClose );
+       
+       //elem.attr("filter", svgBlur)
+      // Snap.animate( 0, .5, function( value ) { filterAnimRef.attributes[0].value = value + ',' + value;  }, 1000, mina.elastic );	
         
+        }); 
+   
+});
         
-        
-    
-
-
-
-}
-
-function closeNav() {
   
-    navOpen = 0;
-    $('.burger').show();
-      $('.close-icon').hide();
-   //$('.burger').attr("class", "burger navClosed");
-     $("#hud-copy").delay(10).fadeOut(100)
-     $(".nav_x002D_pad.on").delay(500).attr("class", "nav_x002D_pad");
-    $(".nav_x002D_pad_selected.on").delay(2000).attr("class", "nav_x002D_pad_selected");
+    //burger.attr({visibility: "hidden"});
+       
+      //  g.attr({
+   // fill: "#bada55",
+   //stroke: "#000",
+   // strokeWidth: 0.4
+       // });
+        
+        //g.transform('s0.5,0.5');
         
         
-        
-        
-
+    
+        });
+    
+   
+       
     
 }
-
-//OPEN CLOSE NAV
-$(".burger-hit").click(function(){
-
-
-    
- if( navOpen == 0)  { openNav() } else { closeNav()};
-
-
-
-});
-
-
-
-
-//TAB TEST
-
-$(".nav_x002D_pad").click(function(){
-    
-    // Instead of .removeClass("newclass")
-$(".nav_x002D_pad_selected").attr("class", "nav_x002D_pad on");
-    
-  // Instead of .addClass("newclass")
-$(this).attr("class", "nav_x002D_pad_selected on");
-
-   // $(".nav_x002D_pad").css("fill","#022222");
-   //$(this).className = "nav_x002D_pad selected";
-    
- //   css("fill","#d0c220");
-    
-});
     
 function ig3() {
  
