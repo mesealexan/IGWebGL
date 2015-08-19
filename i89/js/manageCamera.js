b9ab6421d57f6992fe0112f1df53ed958deec3fd
Radu Milici <radumilici@gmail.com>
2015-08-19 13:12:40 +0300

    sync

--- i89/js/manageCamera.js
+++ i89/js/manageCamera.js
@@ -1,63 +1,64 @@
-function modifyCameraUp (degrees) {	
-	var radians = this.degreesToRadians(degrees);
-	var newUp = new THREE.Vector3(0, Math.cos(radians), Math.sin(radians));
-	var up = new THREE.Vector3( 0, 0, 1 );
-	newUp.applyAxisAngle( up, radians );
-	return newUp;
-}
+function cameraControls() {
+	var animation, _this = this;
+	this.frame = -1;
+	this.setSource = function(path){ animation = parseJSON(path); }	
+	this.play = function(from, to){
+		if(!animation) {console.error("animation undefined!"); return;}
+		if(from == undefined) from = this.frame + 1;
+		if(to == undefined) to = animation.frames.length - 1;
 
-var animateCamera = {
-	frame: -1,
-	play: function(from, to){
-		animateCamera.stop(animation_interval);
+		this.stop(this.animation_interval);
+
 		//decrement because check below first increments then returns true
-		animateCamera.frame = from - 1; 
+		this.frame = from - 1; 
 		//decrement because 0 based
 		to--;
-		animation_interval = setInterval(function(){
-		if(animateCamera.checkPlayback(from, to)){
-			var newUp = modifyCameraUp(animation.frames[animateCamera.frame].rollAngle);
-			camera.up.set(newUp.x, newUp.y, newUp.z);
 
-			var lookAt = new THREE.Vector3(
-				animation.frames[animateCamera.frame].target.x, 
-				animation.frames[animateCamera.frame].target.z, 
-				-animation.frames[animateCamera.frame].target.y);
+		this.animation_interval = setInterval(function(){
+			if(_this.checkPlayback(from, to)){
+				var newUp = _this.modifyCameraUp(animation.frames[_this.frame].rollAngle);
+				camera.up.set(newUp.x, newUp.y, newUp.z);
 
-			camera.target = lookAt;
-			camera.lookAt(lookAt);
-			camera.fov = animation.frames[animateCamera.frame].fov + fovModifier;
-			camera.updateProjectionMatrix();
+				var lookAt = new THREE.Vector3(
+					animation.frames[_this.frame].target.x, 
+					animation.frames[_this.frame].target.z, 
+				   -animation.frames[_this.frame].target.y);
 
-			camera.position.set(
-				(animation.frames[animateCamera.frame].position.x),
-				(animation.frames[animateCamera.frame].position.z),
-				-(animation.frames[animateCamera.frame].position.y));
-		}
-		else {camera.lookAt(camera.target); animateCamera.stop();} //reached the end
+				camera.target = lookAt;
+				camera.lookAt(lookAt);
+				camera.fov = animation.frames[_this.frame].fov + fovModifier;
+				camera.updateProjectionMatrix();
+
+				camera.position.set(
+					(animation.frames[_this.frame].position.x),
+					(animation.frames[_this.frame].position.z),
+					-(animation.frames[_this.frame].position.y));
+			}
+			else {camera.lookAt(camera.target); this.stop();} //reached the end
 		},1000/animation.fps)
-	}, 
-	stop: function(){
-		clearInterval(animation_interval);
-	},
-	checkPlayback: function(from, to){
+	};
+
+	this.stop = function(){clearInterval(this.animation_interval)};
+
+	this.checkPlayback = function(from, to){
 		if (from <= to){ //regular playback
-			if (animateCamera.frame < to){ //still has to play
-				animateCamera.frame++;
+			if (this.frame < to){ //still has to play
+				this.frame++;
 				return true;
 			}
 			else return false; //reached the end
 		}	
 		else if (from > to){ //reverse playback
-			if (animateCamera.frame > to){ //still has to play
-				animateCamera.frame--;
+			if (this.frame > to){ //still has to play
+				this.frame--;
 				return true;
 			}
 			else return false; //reached the end	
 		}
-	},
-	tween: function (frame, speed, onComplete) {
-	//position
+	};
+
+	this.tween = function (frame, speed, onComplete) {
+		/**position**/
 		var startPos = camera.position;
 		var destination = new THREE.Vector3(
 			animation.frames[frame].position.x, 
@@ -71,7 +72,8 @@ var animateCamera = {
 	    posTween.to( { x: destination.x, y: destination.y, z: destination.z }, time );
 	    posTween.start();
 	    posTween.onComplete(function() { if(onComplete) onComplete() });
-	//rotation
+
+		/**rotation**/
     	var rollAngle = animation.frames[frame].rollAngle;
     	var newUp = modifyCameraUp(rollAngle);
 
@@ -79,7 +81,8 @@ var animateCamera = {
 	    angleTween.easing(TWEEN.Easing.Cubic.InOut);
 	    angleTween.to( { x: newUp.x, y: newUp.y, z: newUp.z }, time );
 	    angleTween.start();  
-	//target
+
+		/**target**/
 	    var target = new THREE.Vector3(animation.frames[frame].target.x, 
 			animation.frames[frame].target.z, 
 			-animation.frames[frame].target.y);
@@ -89,12 +92,23 @@ var animateCamera = {
 	    targetTween.to( { x: target.x, y: target.y, z: target.z }, time );
 	    targetTween.start();
 	    targetTween.onUpdate( function () { camera.lookAt(camera.target) });
-	//fov
+
+		/**fov**/
 	    var fovTween = new TWEEN.Tween( camera );
 	    fovTween.to( { fov: animation.frames[frame].fov + fovModifier }, time);
 	    fovTween.start();
 	    fovTween.onUpdate( function () { camera.updateProjectionMatrix() }); 
-	}	
+	};
+
+	this.modifyCameraUp = function (degrees) {	
+		var radians = this.degreesToRadians(degrees);
+		var newUp = new THREE.Vector3(0, Math.cos(radians), Math.sin(radians));
+		var up = new THREE.Vector3( 0, 0, 1 );
+		newUp.applyAxisAngle( up, radians );
+		return newUp;
+	};
+
+	this.degreesToRadians = function (deg) { return deg * (Math.PI / 180); }
 }
 
 var manageCameraAnimations = {
@@ -107,6 +121,4 @@ function setInitialCameraPos () {
 	//position camera at start, no jitter when playback starts
 	animateCamera.play(0, 1);
 }
-
-function degreesToRadians (deg) { return deg * (Math.PI / 180); }
 
