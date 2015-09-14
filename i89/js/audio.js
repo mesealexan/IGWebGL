f6a5c451946d0628b9b49edd32b5c0a400a5ec72
Radu Milici <radumilici@gmail.com>
2015-09-14 17:12:11 +0300

    first audio clip working in chrome

--- i89/js/audio.js
+++ i89/js/audio.js
@@ -8,6 +8,18 @@
 
 // var buttonPressSound = new Howl({ src: ['media/audio/036640938_prev_01c.mp3'] });
 // $('.btn').on('click', function(){buttonPressSound.stop().play();})
+function returnFormatsArray(name){
+    var pre = 'media/audio/' + name;
+    return [pre + '.mp3', pre + '.ogg', pre + '.m4a'];
+}
+
+Howler.iOSAutoEnable = true;
+
+var sound = new Howl({
+    src: ['media/audio/i89-coldnight-intro.ogg',
+        'media/audio/i89-coldnight-intro.m4a',
+        'media/audio/i89-coldnight-intro.mp3']//returnFormatsArray('i89-coldnight-intro')
+});
 
 
 
