d83288acfcd1eb2367dae269190f2abfaeba4c07
bloodbrand <moguay@gmail.com>
2015-10-25 07:44:34 +0200

    r.js build file

---
+++ dev 2.0/build.js
@@ -0,0 +1,16 @@
+({
+	appDir: "",
+    baseUrl: "js",
+    mainConfigFile: "js/app.js",
+    name: "app",
+    paths:{
+    	"three" : "empty:",
+    	"jquery": "empty:",
+    	"underscore": "empty:", 
+        "orbitControls" : "empty:", 
+        "watch" : "empty:", 
+        "howler" : "empty:",
+        "tween" : "empty:"
+    },
+    out: "build/IGWebGL.min.js"
+})
